b521a17d16f5119be72da741debdfd3ffad37376
Josh Rudesill <joshrudesill@gmail.com>
2024-03-12 15:28:00 -0500

    game logic nearing completion

--- src/parcheesi.js
+++ src/parcheesi.js
@@ -8,15 +8,19 @@ let defaultPlayer = {
   moveBag: [],
   color: "",
   turns: 0,
+  pieceOptions: [[], [], [], []],
 };
 let blocks = {};
+let homeSquares = [5, 22, 39, 56];
 let currentPlayers = [
   {
     pieces: [-1, -1, -1, -1],
     moveBag: [],
     color: "yellow",
+    homeSquare: 5,
     turns: 1,
     pieceOptions: [[], [], [], []],
+    lastPiece: { player: -1, at: -1 },
     canMovePieces: false,
     canRoll: false,
   },
@@ -24,8 +28,10 @@ let currentPlayers = [
     pieces: [-1, -1, -1, -1],
     moveBag: [],
     color: "blue",
+    homeSquare: 22,
     turns: 1,
     pieceOptions: [[], [], [], []],
+    lastPiece: { player: -1, at: -1 },
     canMovePieces: false,
     canRoll: false,
   },
@@ -33,8 +39,10 @@ let currentPlayers = [
     pieces: [-1, -1, -1, -1],
     moveBag: [],
     color: "red",
+    homeSquare: 39,
     turns: 1,
     pieceOptions: [[], [], [], []],
+    lastPiece: { player: -1, at: -1 },
     canMovePieces: false,
     canRoll: false,
   },
@@ -42,8 +50,10 @@ let currentPlayers = [
     pieces: [-1, -1, -1, -1],
     moveBag: [],
     color: "green",
+    homeSquare: 56,
     turns: 1,
     pieceOptions: [[], [], [], []],
+    lastPiece: { player: -1, at: -1 },
     canMovePieces: false,
     canRoll: false,
   },
@@ -57,22 +67,26 @@ const parseGameIntoMemory = (piecePostitions, numPlayers, turn) => {
   gameState.numPlayers = numPlayers;
   gameState.turn = turn;
   // Parse db result into array
-  const playerArrays = piecePostitions
+  const gameNotation = piecePostitions.split("#");
+  const lastAt = gameNotation[1]
+    .split("+")
+    .map((i) => i.split(",").map((j) => Number(j)));
+  console.log(lastAt);
+  const playerArrays = gameNotation[0]
     .split("+")
     .map((i) => i.split(",").map((j) => Number(j)));
   setup(numPlayers);
   // Find blocks
   for (let i = 0; i < gameState.numPlayers; i++) {
-    let doubles = {};
-    playerArrays[i].forEach((x) => {
-      doubles[x] = (doubles[x] || 0) + 1;
-      if (doubles[x] === 2) {
-        blocks[x] = i;
-      }
-    });
+    // playerArrays[i].forEach((x) => {
+    //   doubles[x] = (doubles[x] || 0) + 1;
+    //   if (doubles[x] === 2) {
+    //     blocks[x] = i;
+    //   }
+    // });
     currentPlayers[i].pieces = playerArrays[i];
+    currentPlayers[i].lastPiece = { player: lastAt[i][0], at: lastAt[i][1] };
   }
-  console.log(blocks);
 };
 const setup = (numPlayers) => {
   // Error checking
@@ -86,7 +100,7 @@ const setup = (numPlayers) => {
   gameState.numPlayers = numPlayers;
   // Change currentPlayers to match numPlayers
   if (numPlayers !== 4) {
-    currentPlayers.splice(numPlayers - 1, 4 - numPlayers);
+    currentPlayers.splice(numPlayers, 4 - numPlayers);
   }
   board = new Uint8ClampedArray(68).map((_, i) => i);
 };
@@ -95,7 +109,7 @@ const takeTurn = (player) => {
     Math.floor(Math.random() * 5) + 1,
     Math.floor(Math.random() * 5) + 1,
   ];
-  roll = [3, 6];
+  roll = [1, 5];
 
   // Check if any pieces at home still, if so force removal on 5
   if (roll[0] === roll[1]) {
@@ -114,61 +128,83 @@ const takeTurn = (player) => {
 };
 const calculateAllowedMoves = (roll, player) => {
   if (roll.includes(5) && player.pieces.some((p) => p === 0)) {
-    console.log("piece at home");
     for (let i = 0; i < player.pieces.length; i++) {
       if (player.pieces[i] === 0) {
         player.pieceOptions[i] = [-1];
       }
     }
-    return;
-  }
-  for (let j = 0; j < 4; j++) {
-    const piece = player.pieces[j];
-    // console.log("Checking piece at:", piece);
-    if (piece !== 0) {
-      let out = [...roll];
-      for (let i = 0; i < roll.length; i++) {
-        // console.log("Checking roll:", roll[i]);
-        for (let x = 1; x <= roll[i]; x++) {
-          const next = getSquare(piece, x);
-          //   console.log("Next square is:", next);
-          if (blocks[next]) {
-            // console.log("Block found at:", next);
-            if (out.length === 1) {
-              out = [];
-            } else {
-              out.splice(i, 1);
+  } else {
+    for (let j = 0; j < 4; j++) {
+      const piece = player.pieces[j];
+      // console.log("Checking piece at:", piece);
+      if (piece !== 0) {
+        let out = [...roll];
+        for (let i = 0; i < roll.length; i++) {
+          // console.log("Checking roll:", roll[i]);
+          for (let x = 1; x <= roll[i]; x++) {
+            const next = getSquare(piece, x);
+            //   console.log("Next square is:", next);
+            if (blocks[next]) {
+              // console.log("Block found at:", next);
+              if (out.length === 1) {
+                out = [];
+              } else {
+                out.splice(i, 1);
+              }
+              // console.log("Out is now", out);
+              break;
             }
-            // console.log("Out is now", out);
-            break;
           }
         }
+        //   console.log("Piece options is now:", out);
+        player.pieceOptions[j] = out;
       }
-      //   console.log("Piece options is now:", out);
-      player.pieceOptions[j] = out;
+    }
+  }
+  let doubles = {};
+  for (let i = 0; i < gameState.numPlayers; i++) {
+    currentPlayers[i].pieces.forEach((x) => {
+      if (x !== 0) {
+        if (doubles[x]) {
+          doubles[x].push(i);
+        } else {
+          doubles[x] = [i];
+        }
+        if (doubles[x].length === 2) {
+          blocks[x] = doubles[x];
+        }
+      }
+    });
+  }
+
+  // if there is a block at porch -
+  // check if its a personal block
+  // if so remove -1 from any pieces at home (0)
+  console.log("blocks", blocks);
+  if (blocks.hasOwnProperty(player.homeSquare)) {
+    console.log("block at home");
+    if (
+      blocks[player.homeSquare][0] + 1 === gameState.turn &&
+      blocks[player.homeSquare][1] + 1 === gameState.turn
+    ) {
+      player.pieceOptions = player.pieceOptions.map((po) =>
+        po.length === 1 && po[0] === -1 ? [] : po
+      );
     }
   }
 };
 const makeMove = (move, piece, player) => {
   if (move === -1) {
-    let newSquare;
-    switch (player.color) {
-      case "yellow":
-        newSquare = 5;
-        break;
-      case "blue":
-        newSquare = 22;
-        break;
-      case "red":
-        newSquare = 39;
-        break;
-      case "green":
-        newSquare = 56;
-        break;
-      default:
-        newSquare = 0;
-    }
+    let newSquare = player.homeSquare;
+
     player.pieces[piece] = newSquare;
+    if (
+      player.lastPiece.player !== -1 &&
+      blocks.hasOwnProperty(player.homeSquare)
+    ) {
+      console.log("kicking piece");
+      currentPlayers[player.lastPiece.player].pieces[player.lastPiece.at] = 0;
+    }
     // Check for other players
     for (const p of currentPlayers) {
       if (p.color === player.color) {
@@ -186,7 +222,17 @@ const makeMove = (move, piece, player) => {
     player.moveBag =
       player.moveBag[0] === 5 ? [player.moveBag[1]] : [player.moveBag[0]];
   } else {
+    const enemyHomeSquares = homeSquares.filter((i) => i !== player.homeSquare);
+    console.log(enemyHomeSquares);
     const squareToMove = getSquare(player.pieces[piece], move);
+    if (enemyHomeSquares.includes(squareToMove)) {
+      currentPlayers[
+        homeSquares.findIndex((i) => i === squareToMove)
+      ].lastPiece = {
+        player: gameState.turn - 1,
+        at: piece,
+      };
+    }
     player.pieces[piece] = squareToMove;
     // Check for other players
     for (const p of currentPlayers) {
@@ -206,15 +252,22 @@ const makeMove = (move, piece, player) => {
     player.moveBag =
       player.moveBag[0] === move ? [player.moveBag[1]] : [player.moveBag[0]];
   }
+  player.pieceOptions = defaultPlayer.pieceOptions;
   calculateAllowedMoves(player.moveBag, player);
 };
 
 const getSquare = (start, amount) => {
   return (start + amount) % 68;
 };
-parseGameIntoMemory("0,0,5,6+0,12,0,0+0,0,0,0+0,0,0,0", 2);
+parseGameIntoMemory(
+  "0,0,5,0+5,22,0,0+0,0,0,0+0,0,0,0#1,0+-1,-1+-1,-1+-1,-1",
+  2,
+  1
+);
+
 takeTurn(currentPlayers[0]);
 console.log(currentPlayers[0]);
-makeMove(6, 3, currentPlayers[0]);
+makeMove(-1, 3, currentPlayers[0]);
 console.log(currentPlayers[0]);
 console.log(currentPlayers[1]);
+console.log(blocks);
